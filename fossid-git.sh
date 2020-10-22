465f862c8638f31c33816aef1fff61778d27cbaf
Junsu Lee <jslee@opensource.or.kr>
2020-10-23 00:16:18 +0900

    Update fossid-git.sh
    Modify indentations

--- fossid-git.sh
+++ fossid-git.sh
@@ -28,62 +28,81 @@ git_url=$7
 git_branch=$8
 
 print_usage() {
-	echo "Usage: use appropriate params"
+	echo "Usage: please use appropriate params. see README.md"
 	exit 2
 }
 
 function main() {
-	create_project
 
-	if [ -z "$git_url" ];
-	then
-		create_scan
-	else
-		echo "Create scan from git..."
-		create_scan_from_git
-		download_from_git
-		echo "Download finished."
-	fi
+	cat << "EOF"
 
-	run_scan
+                 ./oyhhhhyo/.                                                   
+   -++++++++   -sds+//:`.-/sds-     -/+++/-    .:+++/:`    :/-   -/////:-.      
+   od+------  ods:+oo/-     .sd+  `yh+:-:+hy  odo:--/yd.  `sso   oso///+oso/`   
+   od:       +do:o+.          sd/ :ds     ``  hd`     `   `sso   os+     .oso   
+   odo/////` hd.oo`           .dh  /yyo/-.    -shs/:.     `sso   os+      -ss-  
+   odo:::::` hd.              .dh    `-:+yh+    `.:/shs.  `sso   os+      -ss-  
+   od:       +ds              sd+         od:        -ds  `sso   os+     .oso   
+   od:        ods.          .sd+  /s+/::/ohy`.so/:::+yh:  `sso   oso///+oso:    
+   -/`         -sdy/-.``.-/ydd+    -:/++/:.   .:/+++/-     -/.   -/////:-.      
+                 ./oyhhhhyo/-/hhyo.                                             
+                              +dddds-                                           
+                               `+hddds`               GIT INTEGRATION by OSBC                                      
+                                 `+hds`
 
+EOF
+
+create_project
+
+if [ -z "$git_url" ];
+then
+	create_scan
+else
+	echo "Create scan from git..."
+	create_scan_from_git
+	download_from_git
+	echo "Download finished."
+fi
+
+run_scan
+
+scan_finished=$(get_scan_status)
+echo $scan_finished
+while [ ${scan_finished} -eq 0 ]; do
+	echo "Scanning... $(get_scan_progress)"
 	scan_finished=$(get_scan_status)
-	echo $scan_finished
-	while [ ${scan_finished} -eq 0 ]; do
-		echo "Scanning... $(get_scan_progress)"
-		scan_finished=$(get_scan_status)
-		sleep 2
-		if [[ ${scan_finished} -eq 1 ]]; then
-			break
-		fi
-	done
+	sleep 2
+	if [[ ${scan_finished} -eq 1 ]]; then
+		break
+	fi
+done
 
-	echo -e "----------\n"
-	get_finished_information
-	echo "----------"
-	echo "Finished."
+echo -e "----------\n"
+get_finished_information
+echo "----------"
+echo "Finished."
 
 }
 
 function create_project()
 {
-curl -s POST \
--H "Content-Type: application/json" \
--d "$(cat <<EOT
+	curl -s POST \
+		-H "Content-Type: application/json" \
+		-d "$(cat <<EOT
 {
 "group": "projects", "action": "create", 
 "data": {"username": "$id", "key": "$key", "project_code": "$pname", "project_name": "$pname"}
 }
 EOT
 )" \
-$url | jq "."
+	$url | jq "."
 }
 
 function create_scan()
 {
-curl -s POST \
--H "Content-Type: application/json" \
--d "$(cat <<EOT
+	curl -s POST \
+		-H "Content-Type: application/json" \
+		-d "$(cat <<EOT
 {
 "group": "scans", "action": "create", 
 "data": {"username": "$id", "key": "$key", "project_code": "$pname", "scan_code": "$sname", "scan_name": "$sname", 
@@ -91,13 +110,13 @@ curl -s POST \
 }
 EOT
 )" \
-$url | jq "."
+	$url | jq "."
 }
 
 function create_scan_from_git() {
-curl -s POST \
--H "Content-Type: application/json" \
--d "$(cat <<EOT
+	curl -s POST \
+		-H "Content-Type: application/json" \
+		-d "$(cat <<EOT
 {
 "group": "scans", "action": "create", 
 "data": {"username": "$id", "key": "$key", "project_code": "$pname", "scan_code": "$sname", "scan_name": "$sname", 
@@ -105,13 +124,13 @@ curl -s POST \
 }
 EOT
 )" \
-$url | jq "."
+	$url | jq "."
 }
 
 function download_from_git() {
-curl -X POST \
--H "Content-Type: application/json" \
--d "$(cat <<EOT
+	curl -X POST \
+		-H "Content-Type: application/json" \
+		-d "$(cat <<EOT
 
 {
 "group": "scans", "action": "download_content_from_git", 
@@ -120,66 +139,66 @@ curl -X POST \
 
 EOT
 )" \
-$url | jq "."
+	$url | jq "."
 }
 
 function run_scan() {
-curl -s POST \
--H "Content-Type: application/json" \
--d "$(cat <<EOT
+	curl -s POST \
+		-H "Content-Type: application/json" \
+		-d "$(cat <<EOT
 {
 "group": "scans", "action": "run", 
 "data": {"username": "$id", "key": "$key", "scan_code": "$sname"}
 }
 EOT
 )" \
-$url | jq "."
+	$url | jq "."
 }
 
 function get_scan_status() {
-curl -s POST \
--H "Content-Type: application/json" \
--d "$(cat <<EOT
+	curl -s POST \
+		-H "Content-Type: application/json" \
+		-d "$(cat <<EOT
 {
 "group": "scans", "action": "check_status", 
 "data": {"username": "$id", "key": "$key", "scan_code": "$sname", "type": "SCAN"}
 }
 EOT
 )" \
-$url | jq '.data | .is_finished' -r
-return
+	$url | jq '.data | .is_finished' -r
+	return
 }
 
 function get_scan_progress() {
-curl -s POST \
--H "Content-Type: application/json" \
--d "$(cat <<EOT
+	curl -s POST \
+		-H "Content-Type: application/json" \
+		-d "$(cat <<EOT
 {
 "group": "scans", "action": "check_status", 
 "data": {"username": "$id", "key": "$key", "scan_code": "$sname", "type": "SCAN"}
 }
 EOT
 )" \
-$url | jq '.data | .percentage_done' -r
-return
+	$url | jq '.data | .percentage_done' -r
+	return
 }
 
 function get_finished_information() {
-curl -s POST \
--H "Content-Type: application/json" \
--d "$(cat <<EOT
+	curl -s POST \
+		-H "Content-Type: application/json" \
+		-d "$(cat <<EOT
 {
 "group": "scans", "action": "get_scan_log",
 "data": {"username": "$id", "key": "$key", "scan_code": "$sname"}
 }
 EOT
 )" \
-$url | jq '.data[0] | .date + "\n" + .action + "\n" + .reference + "\n" + .description + "\n" + .comment' -r
-return
+	$url | jq '.data[0] | .date + "\n" + .action + "\n" + .reference + "\n" + .description + "\n" + .comment' -r
+	return
 }
 
 if [ $# -eq 0 ]; then
-    print_usage
+	print_usage
 else
 	main
 fi
